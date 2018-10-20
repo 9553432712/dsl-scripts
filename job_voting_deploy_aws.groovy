60be7323389fe467dc7f3c355d952f81da36a81f
mahesh punugupati <mahesh.rvrjc@gmail.com>
2018-10-20 12:54:26 +0530

    job for voing application

---
+++ job_voting_deploy_aws.groovy
@@ -0,0 +1,29 @@
+
+1.times{
+	job('voting_deploy_aws.groovy'+it) {
+	//ogRotator(int daysToKeep, int numToKeep)
+    logRotator {
+        numToKeep(15)
+    }
+    jdk('Java 8')
+    scm {
+      git {
+           remote {
+               url('https://github.com/9553432712/dsl-scripts.git')
+               credentials('git-credential')
+           }
+           branch("master")
+      }
+  }
+    triggers {
+        githubPush()
+    }
+    steps {
+        //gradle('clean build')
+    }
+    publishers {
+        archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
+    }
+ }
+}
+
